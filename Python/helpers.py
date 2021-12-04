bc450a35dbc7a080c1bfd8075a697fbf835d9371
yr <youssofr@gmail.com>
2021-12-04 14:05:57 +0200

    updated read functions to parse by line or by character

--- Python/helpers.py
+++ Python/helpers.py
@@ -1,20 +1,28 @@
 file_name_prefix = '../Inputs/'
 
-def read_input_at_once(file_name):
+def read_input_at_once(file_name, unit='line'):
     # handle auto name conversion
     if file_name[-3:] == '.py':
         file_name = file_name[:-3] +'.txt'
     
     # open file and return line seperated data points
     with open(file_name_prefix + file_name) as f:
-        return f.readlines()
+        if unit == 'line':
+            return f.readlines()
+        if unit == 'char':
+            return [[char for char in line] for line in f.readlines()]
 
-def read_input_one_by_one(file_name):
+def read_input_one_by_one(file_name, unit='line'):
     # handle auto name conversion
     if file_name[-3:] == '.py':
         file_name = file_name[:-3] +'.txt'
     
     # open file and return line seperated data points
     with open(file_name_prefix + file_name) as f:
-        for line in f.readlines():
-            yield line
+        if unit == 'line':
+            for line in f.readlines():
+                yield line
+        elif unit == 'char':
+            for line in f.readlines():
+                for char in line:
+                    yield char
