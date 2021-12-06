1a4b171fa3f2483c88ef911f7ad7532a8d883ffd
yr <youssofr@gmail.com>
2021-12-06 23:04:12 +0200

    working recursive solution, still not good enoough

--- Python/Day 6: Lanternfish.py
+++ Python/Day 6: Lanternfish.py
@@ -1,8 +1,14 @@
 import numpy as np
+from math import ceil, floor
+from time import time
 
 import helpers
 
 init_state = np.array(helpers.read_input_at_once(__file__, 'int')).squeeze()
+
+# problem parameters
+cycle = 6
+dev_period = 2
 
 def tree_solve(state, n):
     # print('Initial State:', *state)
@@ -10,9 +16,42 @@ def tree_solve(state, n):
         # update state (direct translation to problem statment)
         state -= 1
         state = np.append(state, np.repeat(8, np.where(state<0, 1, 0).sum()))
-        state = np.where(state<0, 6, state)
+        state = np.where(state<0, cycle, state)
         # print(f'Day {i}:', *state)
     return state.size
 
 
-print(tree_solve(init_state, 80))
+def recur_solve(state, n):
+    # function to count offspring over a life span period
+    def descendants(period):
+        # direct descendants of a member over period
+        d = int(ceil(period/(cycle+1)))
+        # print(period, d)
+        # descendants of descendants
+        for p in range(period-(cycle + dev_period + 1), 0, -(cycle+1)):
+            d += descendants(p)
+        return d
+
+    # initial population size
+    population = state.size
+    # count of lanterns in each state
+    states, counts = np.unique(state, return_counts=True)
+
+    # count their offspring
+    for s, c in zip(states, counts): 
+        population += c * descendants(n - s)
+
+    return population
+
+def numer_solve(state, n):
+    pass
+
+observation_period = 150
+
+timer = time()
+print(tree_solve(init_state.copy(), observation_period), end='')
+print(' in time {}'.format(time()-timer))
+
+timer = time()
+print(recur_solve(init_state.copy(), observation_period), end='')
+print(' in time {}'.format(time()-timer))
